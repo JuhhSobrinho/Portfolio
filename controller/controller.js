4de601502a7672a6cf88e5b74a3465c666cfe23c
juliano <juliano.sobrinhojunior@gmail.com>
2025-05-21 21:41:05 -0300

    um portfolio de cara nova

--- controller/controller.js
+++ controller/controller.js
@@ -39,10 +39,6 @@ carregarBancoDeDados()
                     <h1 class="nome-project">${bd[geTLocalStorage("AtualGuia")].nome}</h1>
                     <img  class="img-project" id="img-project" src="${bd[geTLocalStorage("AtualGuia")].img}" alt="foto-do-projeto-${bd[geTLocalStorage("AtualGuia")].nome}">
                 </a>
-                <h1 class="sobre-project-titulo">Sobre o Projeto</h1>
-                <p class="sobre-project-dados">
-                    ${bd[geTLocalStorage("AtualGuia")].sobre}
-                </p>
             </section>
 
             <div class="line-project"></div>
@@ -57,13 +53,20 @@ carregarBancoDeDados()
                 <div class="line-splash"></div>
                 <span class="ficha-spans" id="responsividade">Responsivo: ${bd[geTLocalStorage("AtualGuia")].responsivo}</span>
                 <a href="${bd[geTLocalStorage("AtualGuia")].linkProj}">
-                    <span class="ficha-spans" id="projectLink">Project:  ${bd[geTLocalStorage("AtualGuia")].linkProjFake}</span>
+                    <span class="ficha-spans" id="projectLink">Ver Projeto:  ${bd[geTLocalStorage("AtualGuia")].linkProjFake}</span>
                 </a>
+                
             </div>
+            <p>Repositório GitHub</p>
             <a  href="${bd[geTLocalStorage("AtualGuia")].linkCod}">
             <img class="git-readme" src="https://github-readme-stats.vercel.app/api/pin/?username=JuhhSobrinho&repo=${bd[geTLocalStorage("AtualGuia")].nome}&theme=noctis_minimus" alt="Readme Card">
           </a>
+                          <h1 class="sobre-project-titulo">Sobre o Projeto</h1>
+                <p class="sobre-project-dados">
+                    ${bd[geTLocalStorage("AtualGuia")].sobre}
+                </p>
             </div>
+            
             <div class="line-project"></div>`;
             imgFicha.innerHTML = sectionImgFicha;
         }
@@ -78,16 +81,17 @@ carregarBancoDeDados()
 
 
 
-        bd.forEach(element => {
+        bd.slice().reverse().forEach(element => {
             console.log(element);
 
             textGuiaHTML += `
-        <div class="line" id="line-menu"></div>
-        <a class="guia" id="${element.id}">
-            <img id="${element.nome}" class="icon-guias" src="${element.icon}" style="${saturacion(element.status)}" alt="guia-${element.nome}" />
-        </a>`;
-
+    <a class="guia" id="${element.id}">
+        <img id="${element.nome}" class="icon-guias" src="${element.icon}" style="${saturacion(element.status)}" alt="guia-${element.nome}" />
+        <img loading="lazy" id="img-manu${element.nome}" class="img-projects" src="${element.img}" />
+        <p class="habilidades-dados"> ${element.nome}</p>
+    </a>`;
         });
+
         guias.innerHTML = textGuiaHTML;
 
 
@@ -119,29 +123,52 @@ console.log(geTLocalStorage("AtualGuia"));
 
 
 
-document.addEventListener("DOMContentLoaded", function () { // Garantir que o DOM esteja carregado
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("contact-form").addEventListener("submit", function (event) {
-        event.preventDefault(); // Evita o reload da página
+        event.preventDefault();
 
         const formData = new FormData(this);
 
-        fetch("https://formsubmit.co/juliano.sobrinhojunior@gmail.com", { // Substitua pelo seu e-mail
+        fetch("https://formsubmit.co/juliano.sobrinhojunior@gmail.com", {
             method: "POST",
             body: formData
         })
-        .then(response => {
-            if (response.ok) {
-                alert("Mensagem enviada com sucesso!");
-                document.getElementById("contact-form").reset(); // Limpa o formulário
-            } else {
-                alert("Erro ao enviar mensagem!");
-            }
-        })
-        .catch(error => {
-            alert("Erro ao enviar mensagem: " + error);
-        });
+            .then(response => {
+                if (response.ok) {
+                    Toastify({
+                        text: "Mensagem enviada com sucesso!",
+                        duration: 3000,
+                        gravity: "top", // top or bottom
+                        position: "right", // left, center or right
+                        backgroundColor: "#1B272D", // verde sucesso
+                        close: true
+                    }).showToast();
+
+                    document.getElementById("contact-form").reset();
+                } else {
+                    Toastify({
+                        text: "Erro ao enviar mensagem!",
+                        duration: 3000,
+                        gravity: "top",
+                        position: "right",
+                        backgroundColor: "#f44336", // vermelho erro
+                        close: true
+                    }).showToast();
+                }
+            })
+            .catch(error => {
+                Toastify({
+                    text: "Erro ao enviar: " + error.message,
+                    duration: 3000,
+                    gravity: "top",
+                    position: "right",
+                    backgroundColor: "#f44336",
+                    close: true
+                }).showToast();
+            });
     });
 });
+
 
 
 
@@ -166,11 +193,72 @@ function main() {
                 // Após formar a palavra alvo, aguarde por um segundo antes de trocar para 'Hello World'
                 setTimeout(function () {
                     trocarLetras(0, olamundo);
-                }, 200);
+                }, 100);
             }
-        }, 120); // Ajuste o tempo conforme necessário
+        }, 100); // Ajuste o tempo conforme necessário
     }
 
     trocarLetras(0, test);
 
 }
+
+
+  particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle"
+      },
+      opacity: {
+        value: 0.5,
+        random: false
+      },
+      size: {
+        value: 3,
+        random: true
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 4
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        }
+      },
+      modes: {
+        repulse: {
+          distance: 100
+        },
+        push: {
+          particles_nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+
